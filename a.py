69663a2de6c051841078aa002ceaebc92d92c041
andronikmk <andronikmk@gmail.com>
2020-07-22 12:08:30 -0400

    Add day 1

---
+++ a.py
@@ -0,0 +1,23 @@
+# Given the following array of values, print out all the elements in reverse order, with each element on a new line.
+# For example, given the list
+# [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+# Your output should be
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+
+a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+# my_list_2 = ["10", "9", "8", "0", "4", "5", "15", "3", "2", "1", "7"]
+for i in a:
+    print(a[-i-1])
